aaff5d05ad79fd3d8084cb01ced273a05f6b1d57
Argon42043 <argon42043@gmail.com>
2022-05-31 14:23:55 +0200

    translated intermediate missions

---
+++ missions/intermediate/02_alias_journal/gshrc/de.sh
@@ -0,0 +1,24 @@
+# Diese Datei wird geladen (oder im Shell-Jargon "gesourced"), wenn
+# das Spiel beginnt. Du kannst es verwenden, um Aliase (und auch
+# andere Dinge) zu speichern, die du möglicherweise über Spielsitzungen
+# hinweg behalten möchtest. Tatsächlich wird ein in der Shell definierter
+# Alias "vergessen", wenn er hier nicht hinzugefügt wird (als Teil von
+# Missionen definierte Aliase werden für dich gespeichert).
+# 
+# Hinweis: Alle mit '#' beginnenden Zeilen in dieser Datei werden ignoriert.
+# Die Standard-Shell verwendet solche Dateien, die normalerweise im
+# Home-Verzeichnis versteckt sind. Für bash befindet sich die Datei unter
+#       ~/.bashrc
+# und für zsh unter
+#       ~/.zshrc
+# (Verändere diese Dateien nicht, da sie von GameShell verwendet werden!)
+
+# Wenn du diese Datei bearbetest, werden die Veränderungen nicht direkt
+# in die aktuelle Shell übernommen. Du kannst aber den folgenden Alias
+# benutzen, um diese zu übernehmen.
+alias reload="source ~/.gshrc"
+
+# Hier sind ein paar Alias-Ideen für dich:
+# - ein Alias "gg" für "gsh goal",
+# - ein Alias "gc" für "gsh check",
+# - ein Alias "editrc" um diese Datei mit nano zu bearbeiten.
